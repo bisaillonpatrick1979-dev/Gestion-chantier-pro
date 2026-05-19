29229971efd480704432f6f85f332056fd6efe44
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-19 14:59:11 -0600

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://xbnaviqfivyumpabkdat.supabase.co',
-  'sb_publishable_WEYUMr8HpTAmqIqbSTfpfA_CKRLfOYo'
+  'https://nsxeveoscpbioayrvlxi.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeGV2ZW9zY3BiaW9heXJ2bHhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMjMzOTgsImV4cCI6MjA5NDc5OTM5OH0.v064Bp2HHah4O6bZ5V4WcCV2yD-xsXoerNLJt7hlVeA'
 )
