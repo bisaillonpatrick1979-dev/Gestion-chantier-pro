a5e30531782391def815d06283c6e2b41ceaa434
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-19 15:27:00 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import OnboardingGuard from '@/components/OnboardingGuard'
 import PWARegister from '@/components/PWARegister'
 import ThemeInjector from '@/components/ThemeInjector'
 import DevTools from '@/components/DevTools'
-import SyncManager from '@/components/SyncManager'
 
 export const metadata: Metadata = {
   title: 'Gestion Chantier Pro',
@@ -59,7 +58,6 @@ export default function RootLayout({
         </main>
         <BottomNav />
         <DevTools />
-        <SyncManager />
       </body>
     </html>
   )
