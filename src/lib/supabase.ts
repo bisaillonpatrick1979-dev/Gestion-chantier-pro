25c16fcf145847946c6095b164404633e7bb0a85
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-20 10:25:36 -0600

    Create supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://nsxeveoscpbioayrvlxi.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zeGV2ZW9zY3BiaW9heXJ2bHhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMjMzOTgsImV4cCI6MjA5NDc5OTM5OH0.v064Bp2HHah4O6bZ5V4WcCV2yD-xsXoerNLJt7hlVeA'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
