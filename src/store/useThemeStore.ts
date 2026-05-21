f042a9ade3855a57a167ab4197028f61941ab05e
Hailite <bisaillonpatrick1979@gmail.com>
2026-05-21 17:12:23 -0600

    Prevent Inferno theme selection

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -2,6 +2,8 @@ import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 import { Theme, getTheme, DEFAULT_THEME_ID } from '@/lib/themes'
 
+const normalizeThemeId = (id: string) => (id === 'inferno' ? DEFAULT_THEME_ID : id)
+
 interface ThemeStore {
   themeId: string
   theme: Theme
@@ -13,12 +15,21 @@ export const useThemeStore = create<ThemeStore>()(
     set => ({
       themeId: DEFAULT_THEME_ID,
       theme: getTheme(DEFAULT_THEME_ID),
-      setTheme: id =>
+      setTheme: id => {
+        const safeId = normalizeThemeId(id)
         set({
-          themeId: id,
-          theme: getTheme(id),
-        }),
+          themeId: safeId,
+          theme: getTheme(safeId),
+        })
+      },
     }),
-    { name: 'gcp-theme' }
+    {
+      name: 'gcp-theme',
+      onRehydrateStorage: () => state => {
+        if (state?.themeId === 'inferno') {
+          state.setTheme(DEFAULT_THEME_ID)
+        }
+      },
+    }
   )
 )
